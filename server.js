1ac40e1eb04a782aad8823dd7be81e356ea692b6
Kat Law <kathleen.law@multiverse.io>
2024-02-05 14:35:58 -0500

    init

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const { db } = require("./server/models");
-const app = require("./server/app");
+const { db } = require('./server/models')
+const app = require('./server/app')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 const init = async () => {
   try {
-    await db.sync();
+    await db.sync()
 
     app.listen(PORT, () => {
-      console.log(`Server listening at http://localhost:${PORT}`);
-    });
+      console.log(`Server listening at http://localhost:${PORT}`)
+    })
   } catch (error) {
     console.error('Error starting server:', error)
   }
-};
+}
 
-init();
+init()
